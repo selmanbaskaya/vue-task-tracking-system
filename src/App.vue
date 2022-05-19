5d029dd73cc4d51d158f8888ff002cc48d383f10
Selman <selmanbaskaya1@gmail.com>
2022-05-19 14:08:13 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-app>
+    <v-navigation-drawer app>
+      navigation-drawer
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      app-bar
+    </v-app-bar>
+
+    <v-main>
+      <v-container fluid>
+        container
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+
+    <v-footer app>
+      footer
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+
+  components: {},
+
+  data: () => ({
+    //
+  }),
+};
+</script>
