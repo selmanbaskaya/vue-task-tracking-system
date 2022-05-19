eb4227f6493f98dc235d3adc6988670766fa24c5
Selman <selmanbaskaya1@gmail.com>
2022-05-19 15:48:52 +0300

    app bar rearranged

--- src/components/AppBar/AppBar.vue
+++ src/components/AppBar/AppBar.vue
@@ -6,27 +6,12 @@
 
     <v-spacer></v-spacer>
 
-    <v-btn icon>
-      <v-icon>mdi-heart</v-icon>
-    </v-btn>
-
-    <v-btn icon>
-      <v-icon>mdi-magnify</v-icon>
-    </v-btn>
-
-    <v-menu left bottom>
-      <template v-slot:activator="{ on, attrs }">
-        <v-btn icon v-bind="attrs" v-on="on">
-          <v-icon>mdi-dots-vertical</v-icon>
-        </v-btn>
-      </template>
-
-      <v-list>
-        <v-list-item v-for="n in 5" :key="n" @click="() => {}">
-          <v-list-item-title>Option {{ n }}</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-menu>
+    <div>
+      Hello, Selman Baskaya
+      <v-btn icon>
+        <v-icon>mdi-account</v-icon>
+      </v-btn>
+    </div>
   </v-app-bar>
 </template>
 
@@ -36,9 +21,9 @@ export default {
   name: "AppBar",
   methods: {
     ...mapActions(["updateDrawerStatus"]),
-    test: function() {
-      this.updateDrawerStatus()
-    }
+    test: function () {
+      this.updateDrawerStatus();
+    },
   },
 };
 </script>
