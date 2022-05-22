16f553b70fcb3944b15d0be86f659af8fc84968b
Selman <selmanbaskaya1@gmail.com>
2022-05-22 14:31:48 +0300

    footer component has been implemented

--- src/App.vue
+++ src/App.vue
@@ -8,19 +8,21 @@
       <v-container fluid> container </v-container>
     </v-main>
 
-    <v-footer app> footer </v-footer>
+    <footer-component />
   </v-app>
 </template>
 
 <script>
 import NavigationDrawer from "./components/NavigationDrawer/NavigationDrawer";
 import AppBar from "./components/AppBar/AppBar";
+import Footer from "./components/Footer/Footer"
 export default {
   name: "App",
 
   components: {
     "navigation-drawer-component": NavigationDrawer,
     "app-bar-component": AppBar,
+    "footer-component": Footer,
   },
 
   data: () => ({
